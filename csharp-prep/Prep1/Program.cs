653dcfa6103a1566e0d8d42bcba6d7a180204217
CraigenChitate <chi22013@byui.edu>
2023-09-14 04:43:36 +0200

    updated prep 1 and frep 2 files

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,33 @@
-using System;
+// using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello Prep1 World!");
+//     }
+// }
+
+  using System;
+
+  class FullName 
+  {
+     static void Main()
+     {
+
+
+        Console.Write("What is your First Name?");
+        string FirstName = Console.ReadLine();
+
+
+
+        Console.Write("What is your Last Name?");
+        string LastName = Console.ReadLine();
+
+        string fullName = $"{LastName}, {FirstName}, {LastName}";
+
+
+        Console.WriteLine($"Your name is {fullName}");
     }
 }
+
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,56 @@
+// using System;
+
+// class Program
+// // {
+// //     static void Main(string[] args)
+// //     {
+// //         Console.WriteLine("Hello Prep2 World!");
+// //     }
+// // }
+
 using System;
 
-class Program
+class Grade
+
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+     static void Main()
+     {
+        Console.WriteLine("Enter the  Percentage: ");
+        double percentage =double.Parse(Console.ReadLine());
+        string letter;
+        
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80 )
+        {
+            letter = "B";
+
+        }
+        else if (percentage >= 70 )
+        {
+             letter = "C";
+        }
+        else if (percentage >= 60 )
+        {
+              letter = "D";
+        }
+        else
+        {
+              letter = "F";
+        }
+
+        Console.WriteLine("Your  grade is:  " + letter);
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations you passed this course");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass this course Please try next time.");
+        }
     }
+
 }
+
